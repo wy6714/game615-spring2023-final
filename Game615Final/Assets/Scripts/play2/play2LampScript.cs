e701fcde8994bd60164568e2c3f7b482f9979626
wy6714 <78013025+wy6714@users.noreply.github.com>
2023-04-22 17:37:14 -0400

    lamp trun on and off

---
+++ Game615Final/Assets/Scripts/play2/play2LampScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class play2LampScript : MonoBehaviour
+{
+    private bool on = false;
+
+    [SerializeField] private Sprite offSprite;
+    [SerializeField] private Sprite onSprite;
+
+    ////////////////////////////////////////////////////////////////////////////
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
+
+
+    void Start()
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+
+    void Update()
+    {
+
+    }
+    private void OnMouseDown()
+    {
+        if (!on)
+        {
+            on = true;
+            spriteRenderer.sprite = onSprite;
+        }
+        else
+        {
+            on = false;
+            spriteRenderer.sprite = offSprite;
+        }
+
+    }
+
+
+}
