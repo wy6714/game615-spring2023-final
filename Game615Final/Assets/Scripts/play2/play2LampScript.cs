8a30f91c119534d9f5eff3fde20c18f73a430b3f
wy6714 <78013025+wy6714@users.noreply.github.com>
2023-04-22 18:28:20 -0400

    picture button and picture animation

--- Game615Final/Assets/Scripts/play2/play2LampScript.cs
+++ Game615Final/Assets/Scripts/play2/play2LampScript.cs
@@ -1,27 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class play2LampScript : MonoBehaviour
 {
     private bool on = false;
+    private bool pictureOn = false;
+    public Button picturebutton;
+    [SerializeField] private Animator LampPictureAni;
+    [SerializeField] private Sprite buttonOffSprite;
+    [SerializeField] private Sprite buttonOnSprite;
 
     [SerializeField] private Sprite offSprite;
     [SerializeField] private Sprite onSprite;
 
     [SerializeField] private SpriteRenderer spriteRenderer;
 
+    [SerializeField] private GameObject Tangram5Obj;
+
 
 
     void Start()
     {
         SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        Tangram5Obj.SetActive(false);
     }
 
 
     void Update()
     {
-
+        if (on && pictureOn)
+        {
+            Tangram5Obj.SetActive(true);
+        }
     }
     private void OnMouseDown()
     {
@@ -36,6 +48,22 @@ public class play2LampScript : MonoBehaviour
             spriteRenderer.sprite = offSprite;
         }
 
+    }
+
+    public void pictureButton()
+    {
+        if (!pictureOn)
+        {
+            picturebutton.image.sprite = buttonOnSprite;
+            pictureOn = true;
+            LampPictureAni.SetBool("isMove", true);
+        }
+        else
+        {
+            picturebutton.image.sprite = buttonOffSprite;
+            pictureOn = false;
+            LampPictureAni.SetBool("isMove", false);
+        }
     }
 
 
