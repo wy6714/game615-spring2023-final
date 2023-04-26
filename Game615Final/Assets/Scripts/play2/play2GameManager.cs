d39185da558ded4ba7e53317678407550436c0ec
wy6714 <78013025+wy6714@users.noreply.github.com>
2023-04-26 17:20:05 -0400

    slot bug fixed

--- Game615Final/Assets/Scripts/play2/play2GameManager.cs
+++ Game615Final/Assets/Scripts/play2/play2GameManager.cs
@@ -6,15 +6,23 @@ public class play2GameManager : MonoBehaviour
 {
     public Animator collectAni;
     [SerializeField] private TransitToTangramScript transitSceneScript;
+    [SerializeField] private GameObject slotTangram;
 
 
     void Start()
     {
-
+        slotTangram.SetActive(false);
     }
     private void OnMouseDown()
     {
         collectAni.SetBool("isCollect", true);
         transitSceneScript.collectnum += 1;
+        Invoke("showInSlot", 0.2f);
+    }
+
+    public void showInSlot()
+    {
+        slotTangram.SetActive(true);
+        gameObject.SetActive(false);
     }
 }
